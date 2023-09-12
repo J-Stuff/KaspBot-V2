996d59d402cfd608cea957009747d68ad097e9b9
J-Stuff <jstuff@j-stuff.net>
2023-09-12 14:37:53 +1200

    update init file with missed name change

--- rotatingstatus/__init__.py
+++ rotatingstatus/__init__.py
@@ -1,7 +1,7 @@
-from .rotatingStatus import AboutCommand
+from .rotatingStatus import RotatingStatus
 from redbot.core import commands
 
 
 
 async def setup(bot:commands.Bot):
-    await bot.add_cog(AboutCommand(bot))
+    await bot.add_cog(RotatingStatus(bot))
